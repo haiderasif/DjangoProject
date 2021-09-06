00bc355bb28a456f1c7ce899091e3afef69d457c
haiderasif <66779716+haiderasif@users.noreply.github.com>
2021-09-06 21:22:57 +0500

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -12,8 +12,9 @@ class Employee(models.Model):
     emp_id = models.IntegerField(db_column='Emp_ID', primary_key=True)  # Field name made lowercase.
     emp_name = models.CharField(db_column='Emp_Name', max_length=20)  # Field name made lowercase.
     social_media = models.CharField(db_column='Social_Media', max_length=30, blank=True, null=True)  # Field name made lowercase.
+    emp_pic = models.CharField(db_column='Emp_Pic', max_length=20)  # Field name made lowercase.
     address = models.CharField(db_column='Address', max_length=220, blank=True, null=True)  # Field name made lowercase.
-    deignation = models.CharField(db_column='Deignation', max_length=40, blank=True, null=True)  # Field name made lowercase.
+    designation = models.CharField(db_column='Designation', max_length=40, blank=True, null=True)  # Field name made lowercase.
     seniority = models.CharField(db_column='Seniority', max_length=20, blank=True, null=True)  # Field name made lowercase.
     joining_date = models.DateField(db_column='Joining_Date', blank=True, null=True)  # Field name made lowercase.
     pay_scale = models.IntegerField(db_column='Pay_Scale', blank=True, null=True)  # Field name made lowercase.
@@ -24,3 +25,4 @@ class Employee(models.Model):
     class Meta:
         managed = False
         db_table = 'EMPLOYEE'
+
