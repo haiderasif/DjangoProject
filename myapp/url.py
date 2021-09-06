205e292c51e79737cf10de2cda2838a27f25dff9
haiderasif <66779716+haiderasif@users.noreply.github.com>
2021-09-07 00:31:03 +0500

    Update url.py

--- myapp/url.py
+++ myapp/url.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.index,name="index"),
-    path('counter',views.counter,name='counter'),
-    path('book',views.book,name='book'),
     path('data',views.data,name='data'),
 ]
