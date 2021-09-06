0851304f63313fc91ec5a8e5735a2465856b39a9
haiderasif <66779716+haiderasif@users.noreply.github.com>
2021-09-07 00:20:10 +0500

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -8,6 +8,34 @@ class Booking(models.Model):
     date=models.DateField(max_length=100)
     time=models.TimeField()
     info=models.CharField(max_length=100)
+
+    
+class BoardOfDirector(models.Model):
+    bod_id = models.IntegerField(db_column='BOD_ID', primary_key=True)  # Field name made lowercase.
+    bod_name = models.CharField(db_column='BOD_name', max_length=20)  # Field name made lowercase.
+    bod_tagline = models.CharField(db_column='BOD_TAGLINE', max_length=40, blank=True, null=True)  # Field name made lowercase.
+    bod_quote = models.CharField(db_column='BOD_QUOTE', max_length=200, blank=True, null=True)  # Field name made lowercase.
+    bod_pic = models.CharField(db_column='BOD_PIC', max_length=567, blank=True, null=True)  # Field name made lowercase.
+    bod_qualification = models.CharField(db_column='BOD_QUALIFICATION', max_length=20, blank=True, null=True)  # Field name made lowercase.
+    bod_social = models.CharField(db_column='BOD_Social', max_length=30, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'Board_of_director'
+
+
+class Department(models.Model):
+    dept_id = models.IntegerField(primary_key=True)
+    dept_name = models.CharField(max_length=20, blank=True, null=True)
+    no_of_members = models.IntegerField(blank=True, null=True)
+    project_assigned = models.IntegerField(db_column='Project_assigned', blank=True, null=True)  # Field name made lowercase.
+    dept_location = models.CharField(max_length=20, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'Department'
+
+
 class Employee(models.Model):
     emp_id = models.IntegerField(db_column='Emp_ID', primary_key=True)  # Field name made lowercase.
     emp_name = models.CharField(db_column='Emp_Name', max_length=20)  # Field name made lowercase.
