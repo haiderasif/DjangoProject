4377dae567047f1f301eaa66750df9cd238c5f8d
haiderasif <66779716+haiderasif@users.noreply.github.com>
2021-09-07 23:07:16 +0500

    Changed index.html to data.html

--- myapp/url.py
+++ myapp/url.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('',views.index,name="index"),
-    path('data',views.data,name='data'),
+    path('',views.data,name="data"),
+    path('index',views.index,name='index'),
 ]
